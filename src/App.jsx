3c94fbccc6c08e5df081e698957ca3f55211aee6
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-24 21:00:11 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
+import Aboutm from './Components/Aboutm/Aboutm'
+import Contact from './Components/Contact/Contact'
+import Experience from './Components/Experience/Experience'
+import Footer1 from './Components/Footer1/Footer1'
 import Home from './Components/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
-import Experience from './Components/Experience/Experience'
-import Aboutm from './Components/Aboutm/About'
 import Projects from './Components/Projects/Projects'
-import Contact from './Components/Contact/Contact'
-import Footer1 from './Components/Footer1/Footer1'
 
 
 function App() {
 
   return (
     <>
-    <Home/>
     <Navbar/>
-    <Experience/>
+    <Home/>
     <Aboutm/>
+    <Experience/>
     <Projects/>
     <Contact/>
-    <Footer1/>
+    <Footer1/> 
     </>
   )
 }
