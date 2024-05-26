4518a604793abf760f5beafca643fda3dc052a32
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-26 11:50:26 +0200

    updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,31 +3,6 @@ import { MdOutlineEmail } from 'react-icons/md'
 import { BsGithub } from 'react-icons/bs'
 
 
-const form = document.getElementById('contact-form');
-
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-
-  try {
-    const response = await fetch('/send-message', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      alert('Message sent successfully!');
-      form.reset();
-    } else {
-      alert('Failed to send message. Please try again later.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    alert('An error occurred. Please try again later.');
-  }
-});
-
 const Contact = () => {
   
   return (
